d62ead3d3d437539fbd89cdb071540229ad8eb0f
chrisgrimble <chris.grimble@hmcts.net>
2020-07-09 15:54:21 +0100

    Tidy up code

--- util/parse_ccd.js
+++ util/parse_ccd.js
@@ -5,16 +5,64 @@ function readInCcdFile(fileName) {
   return JSON.parse(rawData);
 }
 
+function arrayDoesNotMatch(eventName, ignoredEvents) {
+  return !ignoredEvents.some(ignoredEvent => {
+    return new RegExp(`^${ignoredEvent}$`).test(eventName);
+  });
+}
+
+function includeEvent(eventName, ignoredEvents) {
+  return arrayDoesNotMatch(eventName, ignoredEvents);
+}
+
+function includeState(preConditionState, postConditionState, ignoredStates) {
+  return arrayDoesNotMatch(preConditionState, ignoredStates) && arrayDoesNotMatch(postConditionState, ignoredStates);
+}
+
+function combineLinks(linksToCombine, combinedLinks, displayTextFunction) {
+  linksToCombine.forEach(link => {
+    const linkKey = `${link.from} --> ${link.to}`;
+    const displayText = ` ${displayTextFunction(link.text)}`;
+    combinedLinks[linkKey] = combinedLinks[linkKey] ? `${combinedLinks[linkKey]}\\n${displayText}` : displayText;
+  });
+}
+
+function createPlantUmlString(unauthorisedStates, roles, combinedLinks) {
+  let plantUmlString = '@startuml\n';
+  plantUmlString += 'hide empty description\n';
+  plantUmlString += 'skinparam state {\n';
+  plantUmlString += 'BackgroundColor<<allStates>> DeepSkyBlue\n';
+  plantUmlString += '  BackgroundColor<<noPermission>> Gainsboro\n';
+  plantUmlString += '  FontColor<<noPermission>> Gray\n';
+  plantUmlString += '}\n';
+  plantUmlString += 'state "All states" as allStates <<allStates>>\n';
+
+  unauthorisedStates.forEach(unauthorisedState => {
+    plantUmlString += `state ${unauthorisedState} <<noPermission>>\n`;
+  });
+
+  if (roles.length > 0) {
+    // force this to be a state diagram
+    plantUmlString += `state "State diagram for ${roles.join(', ')}"\n`;
+  }
+  Object.keys(combinedLinks).forEach(item => {
+    plantUmlString += `${item} : ${combinedLinks[item]}\n`;
+  });
+
+  plantUmlString += '@enduml\n\n';
+  return plantUmlString;
+}
+
 function parseCCD(baseDir, ignoredStates, ignoredEvents, roles, hideUnauthorisedEvents) {
-  const events = readInCcdFile(baseDir + '/CaseEvent.json');
-  const eventAuthorisations = readInCcdFile(baseDir + '/AuthorisationCaseEvent.json');
-  const stateAuthorisations = readInCcdFile(baseDir + '/AuthorisationCaseState.json');
+  const events = readInCcdFile(`${baseDir}/CaseEvent.json`);
+  const eventAuthorisations = readInCcdFile(`${baseDir}/AuthorisationCaseEvent.json`);
+  const stateAuthorisations = readInCcdFile(`${baseDir}/AuthorisationCaseState.json`);
 
   const output = [];
 
   events.forEach(event => {
     const preConditionStates = event['PreConditionState(s)'];
-    const postConditionStates = event['PostConditionState'];
+    const postConditionStates = event.PostConditionState;
     const eventName = event.ID;
 
     const preConditionSatesArray = preConditionStates ? preConditionStates.split(';') : [ '[*]' ];
@@ -26,10 +74,10 @@ function parseCCD(baseDir, ignoredStates, ignoredEvents, roles, hideUnauthorised
       if (includeState(newPreConditionState, postConditionState, ignoredStates)) {
         if (includeEvent(eventName, ignoredEvents)) {
           output.push({
-            "from": newPreConditionState,
-            "to": postConditionState,
-            "text": eventName
-          })
+            from: newPreConditionState,
+            to: postConditionState,
+            text: eventName
+          });
         }
       }
     });
@@ -37,50 +85,36 @@ function parseCCD(baseDir, ignoredStates, ignoredEvents, roles, hideUnauthorised
 
   const outputForRole = (roles.length > 0) ? output.filter(event => {
     return event.text === 'start' || eventAuthorisations.some(eventAuthorisation => {
-      return eventAuthorisation.CaseEventID === event.text &&
-        roles.indexOf(eventAuthorisation.UserRole) >= 0 && eventAuthorisation.CRUD.indexOf('C') >= 0;
+      return eventAuthorisation.CaseEventID === event.text && roles.indexOf(eventAuthorisation.UserRole) >= 0 && eventAuthorisation.CRUD.indexOf('C') >= 0;
     });
   }) : output;
 
   const outputForRoleReadOnly = (roles.length > 0) ? output.filter(event => {
     return event.text === 'start' || eventAuthorisations.some(eventAuthorisation => {
-      return eventAuthorisation.CaseEventID === event.text &&
-        roles.indexOf(eventAuthorisation.UserRole) >= 0 && eventAuthorisation.CRUD.indexOf('C') == -1;
+      return eventAuthorisation.CaseEventID === event.text && roles.indexOf(eventAuthorisation.UserRole) >= 0 && eventAuthorisation.CRUD.indexOf('C') === -1;
     });
   }) : [];
 
   const outputNotForRole = (roles.length > 0) ? output.filter(event => {
     return eventAuthorisations.some(eventAuthorisation => {
-      return eventAuthorisation.CaseEventID === event.text &&
-        roles.indexOf(eventAuthorisation.UserRole) === -1 &&
-        outputForRole.indexOf(event) === -1;
+      return eventAuthorisation.CaseEventID === event.text && roles.indexOf(eventAuthorisation.UserRole) === -1 && outputForRole.indexOf(event) === -1;
     });
   }) : [];
 
   const combinedLinks = {};
-  outputForRole.forEach(link => {
-    combinedLinks[`${link.from} --> ${link.to}`] = combinedLinks[`${link.from} --> ${link.to}`] ?
-      combinedLinks[`${link.from} --> ${link.to}`] + `\\n ${link.text}` :
-      link.text;
+  combineLinks(outputForRole, combinedLinks, text => {
+    return `${text}`;
   });
-
-  const combinedLinksReadOnly = {};
-  outputForRoleReadOnly.forEach(link => {
-    combinedLinks[`${link.from} --> ${link.to}`] = combinedLinks[`${link.from} --> ${link.to}`] ?
-      combinedLinks[`${link.from} --> ${link.to}`] + `\\n ${link.text} (read only)` :
-      `${link.text} (read only)`;
+  combineLinks(outputForRoleReadOnly, combinedLinks, text => {
+    return `${text} (read only)`;
   });
-
   if (!hideUnauthorisedEvents) {
-    const combinedLinksNotForRole = {};
-    outputNotForRole.forEach(link => {
-      combinedLinks[`${link.from} --> ${link.to}`] = combinedLinks[`${link.from} --> ${link.to}`] ?
-        combinedLinks[`${link.from} --> ${link.to}`] + `\\n <strike>${link.text}</strike>` :
-        ` <strike>${link.text}</strike>`;
+    combineLinks(outputNotForRole, combinedLinks, text => {
+      return `<strike>${text}</strike>`;
     });
   }
 
-  let authorisedStates = [];
+  const authorisedStates = [];
   let unauthorisedStates = [];
   if (roles.length > 0) {
     stateAuthorisations.forEach(stateAuthorisation => {
@@ -94,45 +128,12 @@ function parseCCD(baseDir, ignoredStates, ignoredEvents, roles, hideUnauthorised
     });
   }
 
-  unauthorisedStates = unauthorisedStates.filter(unauthorisedState => authorisedStates.indexOf(unauthorisedState) === -1);
+  unauthorisedStates = unauthorisedStates.filter(unauthorisedState => {
+    return authorisedStates.indexOf(unauthorisedState) === -1;
+  });
   unauthorisedStates = [...new Set(unauthorisedStates)];
 
-  var plantUmlString = '@startuml\n';
-  plantUmlString += 'hide empty description\n';
-  plantUmlString += 'skinparam state {\n' +
-    '  BackgroundColor<<allStates>> DeepSkyBlue\n' +
-    '  BackgroundColor<<noPermission>> Gainsboro\n' +
-    '  FontColor<<noPermission>> Gray\n' +
-    '}\n' +
-    'state "All states" as allStates <<allStates>>\n';
-
-  unauthorisedStates.forEach(unauthorisedState => plantUmlString += `state ${unauthorisedState} <<noPermission>>\n`);
-
-  if (roles.length > 0) {
-    plantUmlString += `state "State diagram for ${roles.join(", ")}"\n`; // force this to be a state diagram
-  }
-  Object.keys(combinedLinks).forEach(function (item) {
-    plantUmlString += `${item} : ${combinedLinks[item]}\n`;
-  });
-
-  plantUmlString += '@enduml\n\n';
-
-  //fs.writeFileSync(outputFile, plantUmlString);
-  return plantUmlString;
+  return createPlantUmlString(unauthorisedStates, roles, combinedLinks);
 }
 
-function arrayDoesNotMatch(eventName, ignoredEvents) {
-  return !ignoredEvents.some(ignoredEvent => new RegExp('^' + ignoredEvent + '$').test(eventName));
-}
-
-function includeEvent(eventName, ignoredEvents) {
-  return arrayDoesNotMatch(eventName, ignoredEvents);
-}
-
-function includeState(preConditionState, postConditionState, ignoredStates) {
-  return arrayDoesNotMatch(preConditionState, ignoredStates) && arrayDoesNotMatch(postConditionState, ignoredStates)
-}
-
-module.exports = {
-  parseCCD
-};
+module.exports = { parseCCD };
